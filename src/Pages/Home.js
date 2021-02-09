2253119686a244faa7845d03db6a5edeafa2e0a0
Sam Poppe <poppe.samuel@gmail.com>
2021-02-09 10:29:54 -0800

    adding profile picture to about

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,15 @@ import React from "react";
 function Home() {
   return(
     <div>
-      Homepage
+      <div>
+        Fast Facts:
+
+        Location: San Francisco, CA
+        Skills: Full-stack Software Developer
+        Favorite pokemon: Gyarados
+      </div>
+
+      <img src="/profilepic.jpg"/>
     </div>
 
   )
