066fdc0d25c7a21cbf11e9042753aade7b3599cc
Sam Poppe <poppe.samuel@gmail.com>
2021-05-18 18:50:22 -0700

    added contact form

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -3,13 +3,30 @@ import React from "react";
 function Contact() {
   return(
     <div>
-      <div className="container">
-        Contact me!
+      <section className="card container col-lg-6">
+        <h5 className="card-header">Contact</h5>
+          <figure className="card-body">
+            <form>
+              <div className="form-group">
+                
+                <label for="exampleFormControlInput1">Name</label>
+                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="John Smith"></input>
+              </div>
+
+              <div className="form-group">
+                  <label for="exampleFormControlInput1">Email</label>
+                  <input type="email" className="form-control" id="exampleFormControlInput2" placeholder="example@gmail.com"></input>
+              </div>
+              
+              <div className="form-group">
+                <label for="exampleFormControlTextarea1">Message</label>
+                  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+          </figure>
         
-        by email: poppe.samuel@gmail.com
-        by phone: 608-334-5091
-        
-      </div>
+        </section>
     </div>
 
   )
