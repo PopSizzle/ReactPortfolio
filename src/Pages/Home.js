21e86cb678ddf75f9c6ba8495ecaea56e9d5fb4c
Sam Poppe <poppe.samuel@gmail.com>
2021-06-24 16:59:11 -0700

    fixed pathing to profile pic

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import profilePic from '../Assets/images/profilepic.jpg';
 
 function Home() {
   return (
@@ -6,7 +7,7 @@ function Home() {
       <div className="container">
         <div className="jumbotron">
 
-        <img src="./profilepic.jpg" alt="Sam Poppe"/>
+        <img src={profilePic} alt="Sam Poppe"/>
 
           <h1 className="display-4">Summary</h1>
           <p>My name is Sam Poppe. I am a full stack developer looking for a team where I can make an impact while continuing to learn and grow as a software engineer. As a graduate and teaching assistant of the UC-Berkeley Extension coding program, I am skilled in the use of Javascript, HTML, and CSS. I have experience creating projects which use React, Node, Express, Mongoose, MongoDB, and other technologies. </p>
